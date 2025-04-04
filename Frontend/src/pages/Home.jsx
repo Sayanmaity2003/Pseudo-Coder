7b91da3bb6c0ba74cb88b6f871af91ffdc4ae86b
Soumyadip Karan <soumyadipkaran65@gmail.com>
2025-04-05 02:53:03 +0530

    Added PrescriptionSection Component

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import SpecialityMenu from "../components/SpecialityMenu";
+import PrescriptionSection from "../components/PrescriptionSection";
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialityMenu />
+      <PrescriptionSection />
     </div>
   );
 };
