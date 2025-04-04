841c707147c8f2ae662f08e1ba7b0414ccb1e39d
Sayan Maity <sayanmaity2003@gmail.com>
2025-04-05 03:54:43 +0530

    Added TopDoctors Component

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import SpecialityMenu from "../components/SpecialityMenu";
 import PrescriptionSection from "../components/PrescriptionSection";
+import TopDoctors from "../components/TopDoctors"
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialityMenu />
       <PrescriptionSection />
+      <TopDoctors/>
     </div>
   );
 };
