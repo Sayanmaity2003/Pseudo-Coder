03b1922b4a0efd9c6240f101a7b4dc8b5a20572e
Sayan Maity <sayanmaity2003@gmail.com>
2025-04-05 00:21:23 +0530

    Navbar added

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+    
+      </Routes>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default App;
